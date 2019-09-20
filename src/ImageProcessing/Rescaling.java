01c5ac85a1893180e71d65577c4e12998ac01ccd
Franck Bourzat <franck.bourzat94@gmail.com>
2019-09-20 14:50:48 +0200

    last commit of the class

--- src/ImageProcessing/Rescaling.java
+++ src/ImageProcessing/Rescaling.java
@@ -1,14 +1,14 @@
 
 /* UnderWater Chat App | Franck Bourzat | IMDEA Network */
 
-/* Rescaling file acording to the need of the uder (height,width) */
+/* Rescaling file acording to the needs of the user (height,width) */
+/* Very effective to reduce file size for file > 50 kb */
 
 package ImageProcessing;
 
 import static View.ScalingOption.height;
 import static View.ScalingOption.width;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
@@ -18,10 +18,9 @@ import javax.imageio.ImageIO;
 
 
 public class Rescaling {
-   public String fileName;
-   public int i = 0 ; 
-    
-    
+   private final String fileName;
+
+  
     public Rescaling(String fileName){
         this.fileName=fileName;
             
@@ -34,25 +33,18 @@ public class Rescaling {
         int intHeight = Integer.parseInt(height);
         int intWidth = Integer.parseInt(width);
                         
-        /*check items....*/
+        /* Processing */
         BufferedImage Image = new BufferedImage( intHeight, intWidth, BufferedImage.TYPE_INT_RGB );
         Graphics2D tGraphics2D = Image.createGraphics(); //create a graphics object to paint to
-        tGraphics2D.setBackground( Color.WHITE );
-        tGraphics2D.setPaint( Color.WHITE );
         tGraphics2D.fillRect( 0, 0, intHeight, intWidth );
         tGraphics2D.setRenderingHint( RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR );
         tGraphics2D.drawImage( image, 0, 0, intHeight, intWidth, null ); //draw the image scaled
                         
-        String ext = "";
-        int point = fileName.lastIndexOf('.');
-        if (i > 0) {
-            ext = fileName.substring(0,point+1);
-        }
         
-        /* regular expression to have the name of the file witout the .extension */
+        /* regular expression to have the name of the file without the .extension */
         String fileNameWithoutExt = fileName.replaceFirst("[.][^.]+$", "");
                            
-                        
+        /* creation of the new file to send */                
         File file = new File("./ChatApp/Files/Rescaled",fileNameWithoutExt.concat(height).concat("x").concat(width).concat(".jpeg"));
         ImageIO.write( Image, "JPG", file ); //write the image to a file            
         return file ; 
