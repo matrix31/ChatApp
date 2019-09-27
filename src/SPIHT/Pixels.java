5c30e9b8c6fc2cfc23a08d3856c747461dec549c
Franck Bourzat <franck.bourzat94@gmail.com>
2019-09-27 16:06:39 +0200

    last commit of class Pixels in package SPIHT

--- src/SPIHT/Pixels.java
+++ src/SPIHT/Pixels.java
@@ -1,30 +1,16 @@
+/* UnderWater Chat App | Franck Bourzat | IMDEA Networks */
 
+/* Image Processing on pixels for the SPIHTalgorithm */
 
-/* Image Processing on pixels */
 package SPIHT;
 
-
-
-import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.DataBufferDouble;
-import java.awt.image.DataBufferInt;
-import java.awt.image.PixelGrabber;
 import java.awt.image.Raster;
-import java.awt.image.WritableRaster;
-import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-
-import javax.imageio.ImageIO;
-
 
 public class Pixels {
   
-    
+
     public Pixels (){
     
     }
@@ -38,6 +24,7 @@ public class Pixels {
         return pixel;
     }
     
+        /* return an array of red pixels of the images divided by 255 */
         public double[] redPixels(int[] Pixels, double max, int height, int width){
             double[] normPixels = new double[height*width];
             int [] RedPix = new int[height*width];
@@ -49,6 +36,8 @@ public class Pixels {
             }
             return normPixels;
         }
+        
+           /* return an array of green pixels of the images divided by 255 */
         public double[] greenPixels(int[] Pixels, double max,int height, int width){
             double[] normPixels = new double[height*width];
             int [] GreenPix = new int[height*width];
@@ -58,6 +47,8 @@ public class Pixels {
             }
             return normPixels;
         }
+        
+         /* return an array of blue pixels of the images divided by 255 */
         public double[] bluePixels(int[] Pixels, double max,int height, int width){
             int [] BluePix = new int[height*width];
             double[] normPixels = new double[height*width];
@@ -69,6 +60,8 @@ public class Pixels {
             
             return normPixels;
         }
+        
+         /* return an array of alpha of the images divided by 255 */
         public double[] alpha(int[] Pixels, double max,int height, int width){
             int [] Alpha = new int[height*width];
             double[] normPixels = new double[height*width];
@@ -81,64 +74,6 @@ public class Pixels {
             return normPixels;
         }
         
-    
-        
-        
-
-        
-    
 }
-
-    
-    
-    
-    
-      
-      
-         //   BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-       
-      //  image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-      
-      /*
-      
-        BufferedImage in = ImageIO.read(file);
-        byte[] pixels = new byte[height*width];
-        pixels = ((DataBufferByte) in.getRaster().getDataBuffer()).getData() ;
-        System.out.println("Pixel :"+height*width+" , "+ "Byte "+pixels.length);
-        return pixels ;
-      */
-      
-/*
-      
-      
-    public int [] ByteToInt(byte[] Byte,int height, int width){
-        
-        int[] PixelToInt = new int [Byte.length];
-       
-        for (int i = 0 ; i< (height*width) / 4 ; i++){
-        
-          byte[] byteArray = new byte[4]; 
-          
-          System.arraycopy(Byte, 4*i, byteArray, 0, 4);
-          
-          ByteBuffer bytebuff = ByteBuffer.allocate(4);
-          bytebuff = ByteBuffer.wrap(byteArray);
-          int Int = bytebuff.getInt();
-          
-          PixelToInt[i] = Int;
-          
-        }
-        System.out.println(Arrays.toString(PixelToInt));
-        return PixelToInt; 
-    }
-    
-    public double intToDouble(int Int){
-        double value = (double) Int;
-        return value;
-    }
-}
-
-
-      */
       
       
